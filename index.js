1771d1d02e7c3f03287150e573ba11951da6d4c5
HoanTV <hoantv@example.com>
2023-01-23 21:44:30 +0700

    add cron

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 var http = require('http');
+const cron = require('node-cron');
+
+// Schedule tasks to be run on the server.
+cron.schedule('*/5 * * * * *', function () {
+    console.log('running a task every minute');
+});
+
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
     res.write('Yo!');
